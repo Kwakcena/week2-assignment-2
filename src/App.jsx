65c91cc8e377d7636100d915af446029f75b1d4c
Kwakcena <rhkrgudwh@gmail.com>
2020-06-15 21:08:35 +0900

    Input 컴포넌트 분리
    Input에 대한 UI를 컴포넌트로 분리한다. 클릭시 어떤 이벤트가 발생하도록
    onClick을 선언한다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-
-function Input() {
-  return null;
-}
+import Input from './Input';
 
 function List() {
   return null;
 }
 
+
 export default function App() {
+  function onClick() {
+    // TODO: 할 일 추가
+  }
+
   return (
     <div>
       <h1>To-do</h1>
-      <Input />
+      <Input onClick={onClick} />
       <List />
     </div>
   );
