a5ceda35a1bf77fd861f14ba32a3eec928ddb1ab
Kwakcena <rhkrgudwh@gmail.com>
2020-06-15 21:37:27 +0900

    입력한 할 일을 추가함
    - react 에서 input의 onChange는 event를 반환하여 onChange가 발생할 때
      마다 event를 이용할 수 있다.
    - 스프레드 연산자로 먼저 객체에 기존의 state를 복사한 뒤 변하는 값만
      지정을 해 주는 방식이 굉장히 많이 사용된다.

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,37 @@ import Page from './Page';
 export default function App() {
   const [state, setState] = useState({
     newId: 100,
+    taskTitle: '',
     tasks: [
       { id: 1, title: '아무 것도 하지 않기 #1' },
       { id: 2, title: '아무 것도 하지 않기 #2' },
     ],
   });
 
-  const { newId, tasks } = state;
+  const { newId, taskTitle, tasks } = state;
 
-  function handleClick() {
+  function handleChangeTitle(event) {
     setState({
+      ...state,
+      taskTitle: event.target.value,
+    });
+  }
+
+  function handleClickAddTask() {
+    setState({
+      ...state,
       newId: newId + 1,
-      tasks: [...tasks, { id: newId, title: 'new Task' }],
+      taskTitle: '',
+      tasks: [...tasks, { id: newId, title: taskTitle }],
     });
   }
 
   return (
     <Page
+      taskTitle={taskTitle}
+      onChangeTitle={handleChangeTitle}
+      onClickAddTask={handleClickAddTask}
       tasks={tasks}
-      onClick={handleClick}
     />
   );
 }
--- src/Page.jsx
+++ src/Page.jsx
@@ -3,11 +3,17 @@ import React from 'react';
 import Input from './Input';
 import List from './List';
 
-export default function Page({ tasks, onClick }) {
+export default function Page({
+  taskTitle, onChangeTitle, onClickAddTask, tasks,
+}) {
   return (
     <div>
       <h1>To-do</h1>
-      <Input onClick={onClick} />
+      <Input
+        value={taskTitle}
+        onChange={onChangeTitle}
+        onClick={onClickAddTask}
+      />
       <List tasks={tasks} />
     </div>
   );
